ffc8f65f9c11f6222b48f40a8d6ed9a5879ea436
Venkatsai414 <40845818+slvsai414@users.noreply.github.com>
2024-07-06 11:55:03 +0530

    Update duplicates_elimination.py

--- duplicates_elimination.py
+++ duplicates_elimination.py
@@ -5,9 +5,6 @@ def eliminating_duplicates(arr):
             i+=1
             arr[i]=arr[j]
     return i+1
-if __name__ == '__main__':
-    arr=[1,1,2,3,3,3,4,5,6,6]
-    a = eliminating_duplicates(arr)
-    for i in range(a):
-        print(arr(i),end='')
-    print()
+
+arr=[1,1,2,3,3,3,4,5,6,6]
+a = eliminating_duplicates(arr)
